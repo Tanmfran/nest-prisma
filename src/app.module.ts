4699dc51375610b673481460bd8fd6c99d7e2e77
Tannor Franks <tannor@Tannors-MacBook-Pro.local>
2024-03-02 23:03:54 -0600

    [CHORE] - add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from "./app.service";
 import { PrismaService } from "./prisma/prisma.service";
 import { FormTemplateModule } from './form-template/form-template.module';
 import {PrismaModule} from "./prisma/prisma.module";
+import {LoggerService} from "./logger/logger.service";
 
 @Module({
   imports: [FormTemplateModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, LoggerService],
 })
 export class AppModule {}
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
+
+@Injectable()
+export class LoggerService {
+    private logger: winston.Logger;
+
+    constructor() {
+        this.logger = winston.createLogger({
+            // Winston configuration
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.printf(({ level, message, timestamp }) => {
+                            return `${timestamp} ${level}: ${message}`;
+                        }),
+                    ),
+                }),
+                // Add other transports as needed
+            ],
+        });
+    }
+
+    log(message: string) {
+        this.logger.log('info', message);
+    }
+
+    error(message: string, trace?: string) {
+        this.logger.error(message, { trace });
+    }
+
+    warn(message: string) {
+        this.logger.warn(message);
+    }
+
+    debug(message: string) {
+        this.logger.debug(message);
+    }
+
+    verbose(message: string) {
+        this.logger.verbose(message);
+    }
+}
