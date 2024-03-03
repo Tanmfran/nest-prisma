1e3cbd2ff53ce25a4f933d3ed8c42b29c90d65e5
Tannor Franks <tannor@Tannors-MacBook-Pro.local>
2024-03-02 21:43:25 -0600

    add swagger

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const db_1 = require("./db");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     await (0, db_1.createDatabase)();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("Example app")
+        .setDescription("The example API description")
+        .setVersion("1.0")
+        .addTag("example")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup("api", app, document);
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { createDatabase } from "./db";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   await createDatabase();
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Example app")
+    .setDescription("The example API description")
+    .setVersion("1.0")
+    .addTag("example")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
